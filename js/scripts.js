ca8fd46f411cfb90bdd5b3b171c7f7cfc697a7fe
Marqis Ramos and Trevor Teigen <trevor@>
2019-06-04 09:23:31 -0700

    Walrus page example

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-show").toggle();
+    $("#walrus-hide").fadeToggle();
+  });
+});
+
+
+fadeIn()
+fadeOut()
+fadeToggle()
+slideDown()
+slideUp()
+slideToggle()
